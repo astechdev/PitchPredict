5e08562bd0452443f7d7a442ab1895b76c36e64e
Scott Aslan <astechdev@gmail.com>
2013-03-01 13:59:48 -0500

    test

--- Services/getPitchOutcomes.php
+++ Services/getPitchOutcomes.php
@@ -25,17 +25,3 @@
         echo $_GET['callback']. '('. json_encode($outcomesArray) . ')';
     }
 ?>
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-29743249-1']);
-  _gaq.push(['_setDomainName', 'pitchpredict.com']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
--- Services/getUserStateVariables.php
+++ Services/getUserStateVariables.php
@@ -10,17 +10,3 @@
     // Wrap and write a JSON-formatted object with a function call, using the supplied value of parm 'callback' in the URL:
     echo $_GET['callback']. '('. json_encode($aStateVariablesObject->expose()) . ')';
 ?>
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-29743249-1']);
-  _gaq.push(['_setDomainName', 'pitchpredict.com']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
