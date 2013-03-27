5c190db32c2da9ce2fe5cbf92d516b63a71f165c
Scott Aslan <astechdev@gmail.com>
2013-03-26 22:59:02 -0400

    facebook

--- WebApp/js/util.js
+++ WebApp/js/util.js
@@ -123,19 +123,20 @@ function onDeviceReady()
     phonegap = 'true';
 //    alert('set phonegap to true');
 
+    FB.init({ appId: gAppID, nativeInterface: CDV.FB, useCachedDialogs: false });
 
-    var authorize_url = "https://www.facebook.com/dialog/oauth/?";
-    authorize_url += "client_id=" + gAppID;
-    authorize_url += "&redirect_uri=http://www.app.pitchpredict.com/login_success.html";
-    authorize_url += "&display=touch";
-    authorize_url += "&state=not_connected";
-    authorize_url += "&response_type=token";
-    authorize_url += "&scope=publish_stream,offline_access";
-        
-    client_browser = window.open(authorize_url, '_blank', 'location=no');
-    client_browser.addEventListener('loadstart', iabLoadStart);
-    client_browser.addEventListener('loadstop', iabLoadStop);
-    client_browser.addEventListener('exit', iabClose);
+//    var authorize_url = "https://www.facebook.com/dialog/oauth/?";
+//    authorize_url += "client_id=" + gAppID;
+//    authorize_url += "&redirect_uri=http://www.app.pitchpredict.com/login_success.html";
+//    authorize_url += "&display=touch";
+//    authorize_url += "&state=not_connected";
+//    authorize_url += "&response_type=token";
+//    authorize_url += "&scope=publish_stream,offline_access";
+//        
+//    client_browser = window.open(authorize_url, '_blank', 'location=no');
+//    client_browser.addEventListener('loadstart', iabLoadStart);
+//    client_browser.addEventListener('loadstop', iabLoadStop);
+//    client_browser.addEventListener('exit', iabClose);
 
     // Register some event listeners
     document.addEventListener("online", onDeviceOnline, false);
